c7c25833cc2b54f8151270f728209d312bfd8406
Mayurghule2 <mayurghule@example.com>
2025-04-19 23:12:58 +0530

    first commit

---
+++ src/backend/FirebaseConfig.ts
@@ -0,0 +1,46 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  doc, 
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  query,
+  where,
+  serverTimestamp,
+  onSnapshot,
+  
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_Firebase_API_KEY,
+  authDomain: "placement-portal-d8843.firebaseapp.com",
+  projectId: "placement-portal-d8843",
+  storageBucket: "placement-portal-d8843.appspot.com",
+  messagingSenderId: "142133720679",
+  appId: "1:142133720679:web:15cbe2d04ffd46cecd06c2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { 
+  auth, 
+  db, 
+  createUserWithEmailAndPassword, 
+  addDoc, 
+  collection,
+  doc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  query,
+  where,
+  serverTimestamp,
+  onSnapshot,
+};
